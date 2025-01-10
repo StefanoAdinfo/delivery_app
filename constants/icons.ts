ffa92fe91c5462bd03d9781e1e589dbd4cd3e9cb
StefanoDaniello <stefanodaniello68@gmail.com>
2025-01-10 15:42:43 +0100

    constant icon and img

---
+++ constants/icons.ts
@@ -0,0 +1,39 @@
+import back from "@/assets/icons/back.png";
+import check from "@/assets/icons/check.png";
+import cutlery from "@/assets/icons/cutlery.png";
+import edit from "@/assets/icons/edit.png";
+import filter from "@/assets/icons/filter.png";
+import google from "@/assets/icons/google.png";
+import help from "@/assets/icons/help.png";
+import home from "@/assets/icons/home.png";
+import lock from "@/assets/icons/lock.png";
+import logout from "@/assets/icons/logout.png";
+import phone from "@/assets/icons/phone.png";
+import search from "@/assets/icons/search.png";
+import settings from "@/assets/icons/settings.png";
+import share from "@/assets/icons/share.png";
+import shopping_cart from "@/assets/icons/shopping-cart.png";
+import star from "@/assets/icons/star.png";
+import user from "@/assets/icons/user.png";
+import warning from "@/assets/icons/warning.png";
+
+export const icons = {
+  back,
+  check,
+  cutlery,
+  edit,
+  filter,
+  google,
+  help,
+  home,
+  lock,
+  logout,
+  phone,
+  search,
+  settings,
+  share,
+  shopping_cart,
+  star,
+  user,
+  warning,
+};
---
+++ constants/images.ts
@@ -0,0 +1,25 @@
+import card_gradient from "@/assets/images/card-gradient.png";
+import carusel1 from "@/assets/images/carusel1.png";
+import carusel2 from "@/assets/images/carusel2.png";
+import carusel3 from "@/assets/images/carusel3.png";
+import favicon from "@/assets/images/favicon.png";
+import icon from "@/assets/images/icon.png";
+import icon2 from "@/assets/images/icon2.png";
+import splash_icon from "@/assets/images/splash-icon.png";
+import user from "@/assets/images/user.png";
+import welcome from "@/assets/images/welcome.png";
+import white_gradient from "@/assets/images/white-gradient.png";
+
+export const images = {
+  card_gradient,
+  carusel1,
+  carusel2,
+  carusel3,
+  favicon,
+  icon,
+  icon2,
+  splash_icon,
+  user,
+  welcome,
+  white_gradient,
+};

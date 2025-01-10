1c312607b40e14228979d0cac0a9be78c2fdaac3
StefanoDaniello <stefanodaniello68@gmail.com>
2025-01-10 15:29:40 +0100

    add img,font,icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
   content: ["./app//*.{js,jsx,ts,tsx}", "./components//*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins-Regular", "sans-serif"],
+        "poppins-bold": ["Poppins-Bold", "sans-serif"],
+        "poppins-extrabold": ["Poppins-ExtraBold", "sans-serif"],
+        "poppins-medium": ["Poppins-Medium", "sans-serif"],
+        "poppins-semibold": ["Poppins-SemiBold", "sans-serif"],
+        "poppins-light": ["Poppins-Light", "sans-serif"],
+      },
+      colors: {
+        primary: "#f52s56",
+        secondary: "FF9500",
+        icons: "DAD9E2",
+        background: "#F5F5F5",
+        headline: "#C1C0C9",
+        body: "#262628",
+        subtitle: "#9B9B9B",
+      },
+    },
   },
   plugins: [],
 };
